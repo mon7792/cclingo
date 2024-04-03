911355702a730f1b2d91cad5418a303aa4296c60
mon7792 <monish7792@gmail.com>
2024-04-03 16:34:59 +0100

    added sidebar

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+type Props = {
+    label: string;
+    iconSrc: string;
+    href: string;
+};
+
+export const SidebarItem = ({ label, iconSrc, href }: Props) => {
+    const pathName = usePathname();
+    const active = pathName === href;
+
+
+    return (
+        <Button
+            variant={active ? "sidebarOutline" : "sidebar"}
+            className="justify-start h-[52px]"
+            asChild
+        >
+            <Link href={href}>
+                <Image src={iconSrc} alt={label} width={32} height={32} className="mr-5" />
+                {label}
+            </Link>
+        </Button>
+    );
+}
---
+++ components/sidebar.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
+type Props = {
+    className?: string;
+}
+
+export const Sidebar = ({ className }: Props) => {
+    return (
+        <div className=
+            {cn("flex h-full lg:w-[256px] lg:fixed top-0 left-0 px-4 border-r-2 flex-col", className)}>
+            <Link href="/learn">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Lingo</h1>
+                </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem label="Learn" iconSrc="/learn.svg" href="/learn" />
+                <SidebarItem label="Leaderboard" iconSrc="/leaderboard.svg" href="/leaderboard" />
+                <SidebarItem label="Quests" iconSrc="/quests.svg" href="/quests" />
+                <SidebarItem label="Shop" iconSrc="/shop.svg" href="/shop" />
+            </div>
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton />
+                </ClerkLoaded>
+            </div>
+        </div>
+    );
+}
